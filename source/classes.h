905c95fbb363f1e277db2dbd02c63a17c5dc4202
goga564 <sanikovskiy12@gmail.com>
2022-04-01 12:01:30 +0300

    №2 header creation

---
+++ source/classes.h
@@ -0,0 +1,51 @@
+#ifndef CLASSES_H_INCLUDED
+#define CLASSES_H_INCLUDED
+class Game{
+    Game(){};
+    ~Game(){};
+    void init();
+    bool is_end;
+    int mineleft;
+
+};
+class Tile{
+    Tile(){};
+    ~Tile(){};
+    bool cellstate;
+    bool minepresence;
+    bool LMBclicked(){
+
+    }
+    bool RMBclicked(){
+
+    }
+    int idtexture;
+};
+class Field{
+    Field(){};
+    ~Field(){};
+};
+class MineCount{
+    MineCount(){};
+    ~MineCount(){};
+    int mineleftfunc();
+};
+class Time{
+    Time(){};
+    ~Time(){};
+    float sessiontime;
+};
+class Settings{
+    Settings(){};
+    ~Settings(){};
+    int gamemode;
+    int windowmode;
+};
+class Stats{
+    Stats(){};
+    ~Stats(){};
+    int wins;
+    int loses;
+    int laps;
+};
+#endif // CLASSES_H_INCLUDED
